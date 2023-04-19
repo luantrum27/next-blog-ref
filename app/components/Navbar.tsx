2503439396087ca306d1bc75d1eaa4e8393dbaa7
luantrum27 <hoangtheluan2016@gmail.com>
2023-04-19 23:59:12 +0700

    update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FaYoutube, FaTwitter, FaGithub, FaLaptop } from 'react-icons/fa';
+import { FaYoutube, FaTwitter, FaGithub } from 'react-icons/fa';
 
 export default function Navbar() {
     return (
@@ -9,16 +9,13 @@ export default function Navbar() {
                     <Link href={'/'} className='text-white/90 no-underline hover:text-white'>Luan Hoang</Link>
                 </h1>
                 <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
-                    <Link className="text-white/90 hover:text-white" href="https://www.youtube.com/@DaveGrayTeachesCode">
+                    <Link className="text-white/90 hover:text-white" href="https://www.youtube.com/channel/UC-RNQWItwPTqkNfn90SNXWg">
                         <FaYoutube />
                     </Link>
-                    <Link className="text-white/90 hover:text-white" href="https://courses.davegray.codes/">
-                        <FaLaptop />
-                    </Link>
-                    <Link className="text-white/90 hover:text-white" href="https://github.com/gitdagray">
+                    <Link className="text-white/90 hover:text-white" href="https://github.com/luantrum27">
                         <FaGithub />
                     </Link>
-                    <Link className="text-white/90 hover:text-white" href="https://twitter.com/yesdavidgray">
+                    <Link className="text-white/90 hover:text-white" href="https://www.facebook.com/htluan2706">
                         <FaTwitter />
                     </Link>
                 </div>
